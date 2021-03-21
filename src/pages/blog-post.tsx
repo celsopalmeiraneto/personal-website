fe1f1cb00c9230b7925fc0d3515e30434d1618a8
celsopalmeiraneto <celsopalmeiraneto@gmail.com>
2021-03-21 19:33:45 +0000

    feat: adds blog post page

---
+++ src/pages/blog-post.tsx
@@ -0,0 +1,43 @@
+import styles from "./blog-post.module.scss";
+
+const BlogPost = () => {
+  return (
+    <div id={styles.container}>
+      <h1>The Title</h1>
+      <div>
+        <p>
+          Cillum aute dolore deserunt incididunt nisi sunt ullamco ullamco.
+          Tempor et eiusmod veniam voluptate anim commodo aliquip. Aliqua irure
+          qui minim tempor laboris anim pariatur amet consequat occaecat magna.
+          Ea tempor sunt qui dolor mollit quis nulla eu ea ullamco. Culpa sit
+          aute esse nisi ullamco eu eu elit non nostrud sunt duis elit. Nisi
+          minim dolore sunt qui anim commodo dolor tempor enim reprehenderit
+          voluptate irure. Dolor amet aute duis sunt tempor ut velit minim
+          ullamco non esse ut excepteur. Magna adipisicing adipisicing
+          consectetur deserunt adipisicing minim sunt pariatur ex sit magna
+          culpa. Qui pariatur deserunt ea ipsum ipsum ea Lorem in pariatur
+          fugiat est sunt. Laboris magna eu aliqua aliqua deserunt tempor tempor
+          eu officia ut elit.
+        </p>
+        <p>
+          Nisi qui magna aliqua pariatur nisi dolor. Ad ullamco elit nostrud ad
+          adipisicing eiusmod proident deserunt sint. Occaecat elit officia ut
+          incididunt est proident proident id excepteur sunt elit deserunt.
+          Nostrud do tempor consectetur ea veniam velit deserunt in ea culpa
+          dolor. Ad sunt occaecat magna amet anim labore quis in sunt irure
+          enim.
+        </p>
+        <p>
+          Fugiat occaecat commodo esse elit labore dolore non duis ex tempor.
+          Aliqua do qui et adipisicing fugiat consectetur commodo sint esse qui
+          deserunt aliqua labore occaecat. Excepteur laboris cupidatat ipsum
+          irure sint ipsum culpa sunt tempor aute duis dolore eiusmod. Commodo
+          nostrud dolor veniam non ad esse amet nostrud cillum nulla. Elit non
+          laboris officia minim incididunt excepteur non duis voluptate quis.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPost;
